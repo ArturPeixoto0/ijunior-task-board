0146c5825c66b238607df28620d9461731a33dd8
Artur Peixoto <arturpeixoto140706@gmail.com>
2026-08-04 13:59:52 -0300

    feat/ configura o server.ts em relação às mudanças feitas no global.d.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,2 +1,9 @@
 import 'dotenv/config' 
 import express from 'express'
+
+if (!process.env.JWT_SECRET) {
+  throw new Error('JWT_SECRET não definido nas variáveis de ambiente')
+}
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL não definido nas variáveis de ambiente')
+}
