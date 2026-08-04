309ec783c0b61c874ce0dfe4b023c4c33582d7b1
Artur Peixoto <arturpeixoto140706@gmail.com>
2026-08-04 13:58:06 -0300

    feat/ configura o global.d.ts

--- api/src/@types/global.d.ts
+++ api/src/@types/global.d.ts
@@ -1,1 +1,21 @@
 import type { Client } from '@prisma/client'
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string
+      DATABASE_URL: string
+      JWT_SECRET: string
+      JWT_EXPIRES_IN: string
+      NODE_ENV: 'development' | 'production' | 'test'
+    }
+  }
+
+   namespace Express {
+    interface Request {
+      client?: Pick<Client, 'id' | 'email'>
+    }
+  }
+}
+
+export {}
