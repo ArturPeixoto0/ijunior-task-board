930ac5242c801c6b6c22aae5e51ea55cc0dc13e5
Artur Peixoto <arturpeixoto140706@gmail.com>
2026-08-06 12:33:57 -0300

    feat/ adiciona o ordensRoutes no expressConfig

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { authRoutes } from '../domains/auth/auth.routes';  
+import { ordensRoutes } from '../domains/service-orders/service-orders.routes';
 import cookieParser from 'cookie-parser'
 
 
@@ -11,6 +12,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use('/auth', authRoutes);
-app.use(cookieParser())
+app.use('ordens', ordensRoutes);
+app.use(cookieParser());
 
 export { app };
