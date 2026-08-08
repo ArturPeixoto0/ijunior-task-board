05e51befd306fa23f6a0e4f4b68e74547d9f1de2
Artur Peixoto <arturpeixoto140706@gmail.com>
2026-08-08 11:03:40 -0300

    fix/ corrige os nomes

--- api/src/domains/clients/clients.routes.ts
+++ api/src/domains/clients/clients.routes.ts
@@ -2,16 +2,16 @@ import { Router } from 'express'
 import { authMiddleware } from '../../middlewares/authMiddleware'
 import { clientController } from './clients.controller' 
 
-const serviceRoutes = Router()
-const ServiceController = new clientController()
+const clientRoutes = Router()
+const ClientController = new clientController()
 
 // Todas as rotas deste router exigem autenticação
-serviceRoutes.use(authMiddleware)
+clientRoutes.use(authMiddleware)
 
-serviceRoutes.get('/',        ServiceController.list.bind( ServiceController))
-serviceRoutes.get('/:id',     ServiceController.specific.bind( ServiceController))
-serviceRoutes.post('/',       ServiceController.create.bind( ServiceController))
-serviceRoutes.put('/:id',     ServiceController.update.bind( ServiceController))
-serviceRoutes.delete('/:id',  ServiceController.delete.bind( ServiceController))
+clientRoutes.get('/',        ClientController.list.bind(ClientController))
+clientRoutes.get('/:id',     ClientController.specific.bind(ClientController))
+clientRoutes.post('/',       ClientController.create.bind(ClientController))
+clientRoutes.put('/:id',     ClientController.update.bind(ClientController))
+clientRoutes.delete('/:id',  ClientController.delete.bind(ClientController))
 
-export { serviceRoutes }
+export { clientRoutes }
