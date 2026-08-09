3f5c821b2be9f478360ed091e28ba0e9ac3da9f2
Artur Peixoto <arturpeixoto140706@gmail.com>
2026-08-09 10:39:42 -0300

    feat/ adiciona app.use(cors())

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config' 
-import express from 'express'
+import { app } from './config/expressConfig'
+import cors from 'cors'
 
 if (!process.env.JWT_SECRET) {
   throw new Error('JWT_SECRET não definido nas variáveis de ambiente')
@@ -7,3 +8,10 @@ if (!process.env.JWT_SECRET) {
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL não definido nas variáveis de ambiente')
 }
+
+app.use(cors());
+const PORTA: number = Number(process.env.PORT);
+
+app.listen(PORTA, () => {
+  console.log(`🚀 Servidor rodando na porta ${PORTA}`);
+});
