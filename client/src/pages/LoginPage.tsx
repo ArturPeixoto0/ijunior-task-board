5d9acb3aefaf670066f5ee0c7bbc74e3c5d273d3
Artur Peixoto <arturpeixoto140706@gmail.com>
2026-08-09 17:44:55 -0300

    style/ estiliza a tela de login

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -38,27 +38,33 @@ export function Login() {
 
   return (
     <div>
-      <h1>iRepair — Login</h1>
+      <h1 className ="text-amber-100 flex items-center justify-center h-10">iRepair — Login</h1>
+      <div className="flex justify-center">
       <form onSubmit={handleSubmit}>
+        <div className="m-2 grid grid-cols-1">
         <input 
           type="email" 
           value={email} 
           onChange={e => setEmail(e.target.value)} 
-          placeholder="Email" 
+          placeholder=" Email" 
           required 
+          className="bg-amber-50 border border-black rounded-sm mb-2"
         />
         <input 
           type="password" 
           value={senha} 
           onChange={e => setSenha(e.target.value)} 
-          placeholder="Senha" 
+          placeholder=" Senha" 
           required 
+          className="bg-amber-50 border border-black rounded-sm mb-2"
         />
         {erro && <p style={{ color: 'red' }}>{erro}</p>}
-        <button type="submit" disabled={carregando}>
+        </div>
+        <button type="submit" disabled={carregando} className="bg-red-800 w-full p-1 border border-black rounded-lg cursor-pointer">
           {carregando ? 'Entrando...' : 'Entrar'}
         </button>
       </form>
+      </div>
     </div>
   )
 }
