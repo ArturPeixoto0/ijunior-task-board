92604c398c37efeb946ef47ef782244fc6ff52c5
Artur Peixoto <arturpeixoto140706@gmail.com>
2026-08-10 17:07:17 -0300

    fix/ corrige o expiresIn poder ser undefined

--- api/src/utils/token.ts
+++ api/src/utils/token.ts
@@ -1,9 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { TokenPayloadCreate, TokenPayloadVerify } from '../@types/payloads'
 
+
+const expiresIn = process.env.JWT_EXPIRES_IN;
+
 export function generateToken (payload: TokenPayloadCreate): string {
   return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    ...(expiresIn && {expiresIn})
   })
 }
 
